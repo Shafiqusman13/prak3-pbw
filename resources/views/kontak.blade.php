08fb6fcabaa72e619768b16a2080d226f3fbb010
Shafiqusman13 <shafiqusmannurhananto@gmail.com>
2025-10-03 10:27:13 +0700

    First commit: upload project Laravel

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kontak - LaraPress</title>
+</head>
+<body>
+    <h1>Hubungi Kami</h1>
+    <p>Jika ada pertanyaan, silakan hubungi kami melalui kontak berikut:</p>
+
+    <ul>
+        <li>Email: kontak111@larapress.com</li>
+        <li>Telepon: +6281312047890</li>
+    </ul>
+</body>
+</html>
+    <a href="/">Kembali ke Halaman Utama</a>
+    <a href="/tentang-kami">Tentang Kami</a>
